084f88356df76a58e8c02f4f2e928079a94f6d7b
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-08-03 19:41:37 +0300

    patch: remove unneeded files

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ cp -r hedera-sdk-js/{lib,package.json,README.md,LICENSE.txt} orig
 
 rm -rf patched
 cp -r orig patched
+
+find patched -name "*.d.ts" -delete
+rm patched/lib/index-web.js
--- patched/lib/generated/NetworkService_pb.d.ts
+++
@@ -1,7 +0,0 @@
-// package: proto
-// file: NetworkService.proto
-
-import * as jspb from "google-protobuf";
-import * as Query_pb from "./Query_pb";
-import * as Response_pb from "./Response_pb";
-
