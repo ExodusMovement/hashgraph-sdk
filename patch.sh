22e127acf3599cc037a0cf34788431770dcfd5d5
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-08-03 20:43:22 +0300

    patch: other way of fixing global usage

--- patch.sh
+++ patch.sh
@@ -6,7 +6,7 @@ cp -r orig patched
 find patched -name "*.d.ts" -delete
 rm patched/lib/index-web.js
 
-replace "var global = Function('return this')();" "const global = jspb.globalScope; const { proto } = jspb.globalScope" -- patched/lib/generated/*_pb.js > /dev/null
+replace "var global = Function('return this')();" "const { proto } = jspb.globalScope" -- patched/lib/generated/*_pb.js > /dev/null
 if grep -qrE "[^a-zA-Z]Function\(" patched; then
   echo "Error: Function( still present"
   exit -1
@@ -17,8 +17,13 @@ replace "require('google-protobuf/google/protobuf/wrappers_pb.js')" "require('..
 replace 'require("google-protobuf")' "require('./google-protobuf.patched/google-protobuf')" -- patched/lib/*.js > /dev/null
 replace 'require("google-protobuf/google/protobuf/wrappers_pb")' "require('./google-protobuf.patched/wrappers_pb')" -- patched/lib/*.js > /dev/null
 replace 'require("google-protobuf/google/protobuf/wrappers_pb")' "require('../google-protobuf.patched/wrappers_pb')" -- patched/lib/*/*.js > /dev/null
+replace ", global)" ", jspb.globalScope)" -- patched/lib/generated/*.js > /dev/null
 cp -r google-protobuf.patched patched/lib/
 if grep -qrE "require\(.google-protobuf" patched; then
   echo "Error: google-protobuf still present"
+  exit -1
+fi
+if grep -qrE '(^|[^a-zA-Z."])global([^a-zA-Z]|$)' patched; then
+  echo "Error: global still present"
   exit -1
 fi
