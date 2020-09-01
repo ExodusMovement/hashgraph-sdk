be07650bcc4084f694911713abbdb9f920beaf90
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-09-01 14:04:38 +0300

    Rebuild

--- orig/lib/crypto/PublicKey.d.ts
+++ orig/lib/crypto/PublicKey.d.ts
@@ -2,5 +2,3 @@ import * as pb from "../generated/BasicTypes_pb";
 export declare abstract class PublicKey {
     abstract _toProtoKey(): pb.Key;
 }
-export declare function _fromProtoKey(key: pb.Key): PublicKey;
-export declare function _fromProtoKeyList(keys: pb.KeyList): PublicKey[];
